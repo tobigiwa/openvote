f6ab10522e6b023a4aa268edecfa573775ed0810
tobigiwa <giwaoluwatobi@gmail.com>
2023-08-26 09:16:08 -0400

    added main.go for simple setup...completed

--- main.go
+++ main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/tobigiwa/openvote/chaincode"
+)
+
+func main() {
+	contract := new(chaincode.SmartContract)
+	cc, err := contractapi.NewChaincode(contract)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := cc.Start(); err != nil {
+		panic(err.Error())
+	}
+}
